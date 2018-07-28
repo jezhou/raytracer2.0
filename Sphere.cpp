9401fe7bdc3661e0cecac1ca50d0cdd1fc45694a
jezhou <jesse.zhou@gusto.com>
2018-07-28 09:59:10 -0700

    clean up sphere

--- Sphere.cpp
+++ Sphere.cpp
@@ -1,14 +1,15 @@
 #include "Sphere.h"
 #include <glm/geometric.hpp>
-#include <iostream>
 
 // This calculates the discriminant of ray/sphere equation.
 // dot(d, d)t ^2 + 2*(d, oc)*t + dot(oc, oc) - radius^2
 // This is a general Quadratic Equation where you can figure out a, b, and c.
 // Calculating the discriminant is enough to check if we hit the sphere or not.
+
 // Discriminant > 0 => 2 hits (back and front of sphere),
 // Discriminant == 1 => 1 hit (side of sphere),
 // Discriminant < 0 => no intersections
+
 bool Sphere::intersection(Ray& ray) {
   glm::vec3 oc = ray.getOrigin() - this->center;
   glm::vec3 d = ray.getDirection();
